a10f92c7d9391d8afa186f1078272b5e6d94f63a
Hatim Hebboul <hebboul.hatim@gmail.com>
2026-04-15 18:38:08 +0200

    add src

---
+++ src/fabric/nb_demo.Notebook/nb_demo.py
@@ -0,0 +1,28 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "",
+# META       "default_lakehouse_name": "",
+# META       "default_lakehouse_workspace_id": "",
+# META       "known_lakehouses": []
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+print("Hello world from Microsoft Fabric notebook!")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
